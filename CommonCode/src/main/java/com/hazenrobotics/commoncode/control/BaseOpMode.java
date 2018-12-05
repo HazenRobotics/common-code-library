f390836724cc10c5580dc6abbea44a6dd60e808e
Zenos5 <AngelJW@outlook.com>
2018-12-04 16:20:41 -0800

    Added BaseOpMode
    Changes:
    fixed problem with phones disconnecting in the initialized state

---
+++ CommonCode/src/main/java/com/hazenrobotics/commoncode/control/BaseOpMode.java
@@ -0,0 +1,66 @@
+package com.hazenrobotics.commoncode.control;
+
+import com.hazenrobotics.commoncode.interfaces.OpModeInterface;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public abstract class BaseOpMode extends LinearOpMode implements OpModeInterface {
+
+    @Override
+    public synchronized void waitForStart() {
+        while (!opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("status", "waiting for start command...");
+            telemetry.update();
+            synchronized (this) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+        }
+        telemetry.clear();
+    }
+
+    @Override
+    public Gamepad getGamepad1() {
+        return gamepad1;
+    }
+
+    @Override
+    public Gamepad getGamepad2() {
+        return gamepad2;
+    }
+
+    @Override
+    public DcMotor getMotor(String name) {
+        return hardwareMap.dcMotor.get(name);
+    }
+
+    @Override
+    public Servo getServo(String name) {
+        return hardwareMap.servo.get(name);
+    }
+
+    @Override
+    public DigitalChannel getDigitalChannel(String name) {
+        return hardwareMap.digitalChannel.get(name);
+    }
+
+    @Override
+    public HardwareDevice get(String name) {
+        return hardwareMap.get(name);
+    }
+
+    @Override
+    public Telemetry getTelemetry() {
+        return telemetry;
+    }
+}
